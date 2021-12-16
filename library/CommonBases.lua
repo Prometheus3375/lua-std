a384ff126d4e08aedfb4ce8b8fdf03c769256d1b
Prometheus3375 <prometheus3375@gmail.com>
2021-12-16 20:40:26 +0300

    Update CommonBases.lua for new interfaces
    Added SupportsLessThan, SupportsGetNumericKey, SupportsSetNumericKey.
    Fixed Sized, now class implements it if len is not a default and
      (value is a function or None or nil).

--- library/CommonBases.lua
+++ library/CommonBases.lua
@@ -1,34 +1,43 @@
-function InitCommonBases(common, Interface)
+function InitCommonBases(common, Class, Interface)
   --region Initialization
   common = common or _ENV.common or _ENV.Common
+  Class = Class or _ENV.Class
   Interface = Interface or _ENV.Interface
 
-  local def_method = Interface.DefineMethod
+  local isNoneOrNil = common.isNoneOrNil
+  local Method = Interface.Method
 
   local CB = {}
   --endregion
 
-  local method = {
-    contains = def_method('__contains', '(self, value)'),
-    iter = def_method('__iter', '(self)'),
-    inext = def_method('__inext', '(self)'),
-    reverse = def_method('__reverse', '(self)'),
-    len = def_method('__len', '(self)', true),
-    call = def_method('__call', '(self, ...)', true),
-    array = def_method('__array', '(self)'),
-  }
+  CB.Callable = Interface('Callable', true, {__call = Method('self, ...', true)})
+  CB.SupportsLessThan = Interface('SupportsLessThan', true, {__lt = Method('self, other', true)})
 
-  CB.Array = Interface('Array', true, {method.array})
-  CB.Callable = Interface('Callable', true, {method.call})
-  CB.Container = Interface('Container', true, {method.contains})
-  CB.Iterable = Interface('Iterable', true, {method.iter})
-  CB.Reversible = Interface('Reversible', true, {method.reverse})
-  -- fixme any instance is sized, because __len is set
-  -- also any class without custom __len must be sized
-  CB.Sized = Interface('Sized', true, {method.len})
-  CB.Collection = Interface('Collection', true, {}, CB.Container, CB.Iterable, CB.Sized)
-  CB.OrderedCollection = Interface('OrderedCollection', true, {}, CB.Collection, CB.Reversible)
-  -- todo add SupportsLessThan, SupportsGetNumericKey, SupportsSetNumericKey
+  CB.Array = Interface('Array', true, {__array = Method('self', false)})
+  CB.Container = Interface('Container', true, {__contains = Method('self, value', false)})
+  CB.Iterable = Interface('Iterable', true, {__iter = Method('self', false)})
+  CB.Reversible = Interface('Reversible', true, {__reverse = Method('self', false)})
+
+  CB.SupportsGetNumericKey = Interface(
+    'SupportsGetNumericKey',
+    true,
+    {__get_numeric_key = Method('self, key', false)}
+  )
+
+  CB.SupportsSetNumericKey = Interface(
+    'SupportsSetNumericKey',
+    true,
+    {__set_numeric_key = Method('self, key, value', false)}
+  )
+
+  local default_len = Class('temp1', {}).__meta.__len
+
+  local function sized_check_imp_absence(func)
+    -- None for checks in Class.meta
+    return func == default_len or (type(func) ~= 'function' and not isNoneOrNil(func))
+  end
+
+  CB.Sized = Interface('Sized', true, {__len = Method('self', true, nil, sized_check_imp_absence)})
 
   local function iterator_iter(self) return self end
   local function iterator_call(self)
@@ -40,14 +49,17 @@ function InitCommonBases(common, Interface)
 
   CB.Iterator = Interface(
     'Iterator',
-    {
-      method.iter:WithDefault(iterator_iter),
-      method.call:WithDefault(iterator_call),
-      method.inext,
+    true, {
+      __iter = Method('self', false, iterator_iter),
+      __call = Method('self', true, iterator_call),
+      __inext = Method('self', false),
     },
     CB.Iterable,
     CB.Callable
   )
+
+  CB.Collection = Interface('Collection', true, {}, CB.Container, CB.Iterable, CB.Sized)
+  CB.OrderedCollection = Interface('OrderedCollection', true, {}, CB.Collection, CB.Reversible)
 
   return setmetatable(CB, common.generate_package_metatable('Common Bases'))
 end
