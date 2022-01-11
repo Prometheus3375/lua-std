214455eed537a189b25df2fda2c520290e945dc3
Prometheus3375 <prometheus3375@gmail.com>
2022-01-11 21:24:29 +0300

    Update checks
    Make all var local.
    Add more checks.

--- misc/checks/eq.lua
+++ misc/checks/eq.lua
@@ -1,10 +1,10 @@
-meta1 = {__eq = function(self, other) print('meta1') return rawequal(self, other) end}
-meta2 = {__eq = function(self, other) print('meta2') return rawequal(self, other) end}
+local meta1 = {__eq = function(self, other) print('meta1') return rawequal(self, other) end}
+local meta2 = {__eq = function(self, other) print('meta2') return rawequal(self, other) end}
 
-t1 = setmetatable({}, meta1)
-t2 = setmetatable({}, meta2)
-t3 = setmetatable({}, meta1)
-t4 = {}
+local t1 = setmetatable({}, meta1)
+local t2 = setmetatable({}, meta2)
+local t3 = setmetatable({}, meta1)
+local t4 = {}
 
 print('t1 == nil', t1 == nil)
 print('t1 == false', t1 == false)
