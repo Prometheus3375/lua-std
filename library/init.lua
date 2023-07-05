60e71d93787a858f48929986fb310e2a44f0758e
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2023-07-05 19:48:33 +0300

    Update init way
    init.lua now must be called via require().
      It will return PLSL table.

--- library/init.lua
+++ library/init.lua
@@ -1,10 +1,30 @@
-function InitPLSL(root_dir)
-  -- Prometheus' Lua Standard Library
-  PLSL = {}
+--[[
 
-  dofile(root_dir .. '/common.lua')
-  dofile(root_dir .. '/Class.lua')
-  dofile(root_dir .. '/CommonBases.lua')
-  dofile(root_dir .. '/CommonEx.lua')
-  dofile(root_dir .. '/hash.lua')
-end
+      Prometheus' Lua Standard Library
+
+--]]
+
+local _, path = ...
+local root = string.sub(path, 1, -9)  -- length of the string 'init.lua'
+
+local old = PLSL
+local library = {}
+PLSL = library
+
+dofile(root .. 'common.lua')
+dofile(root .. 'Class.lua')
+dofile(root .. 'CommonBases.lua')
+dofile(root .. 'CommonEx.lua')
+
+-- todo protect PLSL
+
+PLSL = old
+
+return library
+
+
+-- todo do not use upvalues, use lib functions instead?
+-- todo add format argument to str and __str
+-- todo in all files return values, do not assign them to PLSL
+--   this is necessary for correct linking to docs later
+-- todo remove useless do end
